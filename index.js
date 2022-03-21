36e4db216a6c748fe21d30456cffb173bbb43df1
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-03-21 16:06:53 -0400

    adding new files for the login in system.

--- index.js
+++ index.js
@@ -16,35 +16,62 @@ const  myHome  = () => {
   })
 };
 
-let photoDivs = () => {
-  const bookList = document.getElementsByClassName('fboxitem');
-  const bookPhotos = document.getElementById('nPhotos');
-  console.log('bookPhotos: ', bookPhotos);
-  console.log('bookList: ', bookList);
-};
+// let photoDivs = () => {
+//   const bookList = document.getElementsByClassName('fboxitem');
+//   const bookPhotos = document.getElementById('nPhotos');
+//   console.log('bookPhotos: ', bookPhotos);
+//   console.log('bookList: ', bookList);
+// };
 
-const bookSearch = () => {
-let input, filter, div, books, labels, i, bookList, txtValue;
-input = document.getElementById('search-bar');
-filter = input.style.display.toLowerCase()
-div = document.getElementById('main-content');
-books = document.getElementsByClassName('fboxitem');
-labels = document.getElementsByTagName('label');
-console.log('input: ', input, 'filter: ',filter, 'div: ',div, 'books: ', books, 'labels: ', labels);
+// photoDivs();
 
-  for (i = 0; i < books.length; i++) {
-    bookList = books[0];
-    txtValue = bookList.textContent || bookList.innerHTML;
-    console.log(txtValue);
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      books[i].style.display = " ";
-    } else {
-      books[i].style.display = "none";
-    }
-  }
-}
+// const bookSearch = () => {
+// let input, filter, div, books, labels, i, bookList, txtValue;
+// input = document.getElementById('search-bar');
+// filter = input.style.display.toLowerCase()
+// div = document.getElementById('main-content');
+// books = document.getElementsByClassName('fboxitem');
+// labels = document.getElementsByTagName('label');
+// console.log('input: ', input, 'filter: ',filter, 'div: ',div, 'books: ', books, 'labels: ', labels);
 
-bookSearch();
+//   for (i = 0; i < books.length; i++) {
+//     bookList = books[0];
+//     txtValue = bookList.textContent || bookList.innerHTML;
+//     console.log(txtValue);
+//     if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//       books[i].style.display = " ";
+//     } else {
+//       books[i].style.display = "none";
+//     }
+//   }
+// }
+
+// bookSearch();
+
+// const bookList = document.getElementsByClassName('names');
+// console.log(bookList)
+
+// const searchBar = document.forms['search'].querySelector('input');
+// searchBar.addEventListener('keyup', function(e){
+//   const text = e.target.value.toLowerCase();
+//   const books = bookList.getElementsByTagName('label');
+//   Array.from(books).forEach(function(book){
+//     const title = book.label.textContent;
+//     if(title.toLowerCase().indexOf(text) != -1) {
+//       book.style.display = 'block';
+//     } else {
+//       book.style.display = 'none';
+//     }
+//   });
+// });
+
+
+// const searchBooks = document.querySelector('input');
+// const books = document.getElementsByClassName('fboxitem');
+
+// textValue = searchBooks.value.toLowerCase();
+// console.log('searchBooks: ', searchBooks, 'books: ', books);
+
 
 
 //Adds filter array
@@ -57,4 +84,4 @@ bookSearch();
 
 //Adds hide books from list
 
-//This JavaScript is specifice to the book search query.
+//This JavaScript is specifice to the book search query
